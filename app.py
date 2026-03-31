927424ca2eafd129e1c33f733e0a712844d7041a
adeyharshitha-lang <adeyharshitha@gmail.com>
2026-03-31 12:27:27 +0530

    path change
    Created a comment line

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 # Load the model and label encoder
+# Note: Ensure thet the model
 model=joblib.load("employee_attrition_model.pkl")
 label_encoder=joblib.load("label_encoder.pkl")
 feature_columns=joblib.load("feature_columns.pkl")
